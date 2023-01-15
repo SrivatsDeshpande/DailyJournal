83689df1550adbbbc3cb403689c506f20b633235
srivatsdeshpande <srivatsdeshpande@gmail.com>
2023-01-15 21:36:51 +0530

    static file issue fixed

--- DailyJournal/settings.py
+++ DailyJournal/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-  
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +123,7 @@ AUTH_USER_MODEL = 'base.User'
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
