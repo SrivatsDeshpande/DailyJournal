ccdd1838dbf29ff2f43baf0db4d573f121c3108f
srivatsdeshpande <srivatsdeshpande@gmail.com>
2023-01-15 22:21:56 +0530

    widget_tweaks

--- DailyJournal/settings.py
+++ DailyJournal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
-    'widget-tweaks'
+    'django_widget_tweaks'
 ]
 
 MIDDLEWARE = [
